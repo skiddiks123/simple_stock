36e2d5e3ca38bc4863762346675d1ef292d3d68b
shatoidil <shatoidil@gmail.com>
2017-03-12 19:15:28 +0300

    04 Апдейт файла config добавлен файл db_create, который при запуске генерирует базу. Папка models добавлен класс User. На сервере portal создана база.

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,20 @@
-# Import flask and template operators
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-# Import SQLAlchemy
-# from flask.ext.sqlalchemy import SQLAlchemy
 
-# Define the WSGI application object
 app = Flask(__name__)
 
-# Configurations
-# app.config.from_object('config')
+db = SQLAlchemy(app)
+db.init_app(app)
 
-# Define the database object which is imported
-# by modules and controllers
-# db = SQLAlchemy(app)
-
-# Sample HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
-# Import a module / component using its blueprint handler variable (mod_auth)
-# from app.mod_auth.controllers import mod_auth as auth_module
+# Добавления файла конфигурации 
+config = app.config.from_object('config')
+
 from app.home.controllers import home as home_module
 
-# Register blueprint(s)
-# app.register_blueprint(auth_module)
+from app.models import user
 app.register_blueprint(home_module)
-# ..
-
-# Build the database:
-# This will create the database file using SQLAlchemy
-# db.create_all()
